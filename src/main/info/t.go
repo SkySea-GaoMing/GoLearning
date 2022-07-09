784d909ad96ada08ccfb4ea411b07644fb702d05
SkySea-GaoMing <1849731045@qq.com>
2022-07-09 18:19:24 +0800

    my second commit

--- src/main/info/t.go
+++ src/main/info/t.go
@@ -2,12 +2,6 @@ package main
 
 import "fmt"
 
-func fo(x int, y int) (int, int) {
-	return y, x
-}
-
-var A int = 3
-
 func main() {
 	a := new(int)
 	fmt.Println(a)
--- src/model/utils.go
+++ src/model/utils.go
@@ -1,10 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a int
-	fmt.Print(a)
-	const b = 3
 
 }
